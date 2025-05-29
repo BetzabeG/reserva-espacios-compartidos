d40ec3fca0416c2862adc2f3ecee4fed23c49dcd
Betzabe Gutierrez <betzabegutierrezmorales@gmail.com>
2025-05-28 23:32:07 -0400

    proyecto creado

---
+++ espacio_compartido/src/main/java/com/example/espacio_compartido/EspacioCompartidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.espacio_compartido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacioCompartidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacioCompartidoApplication.class, args);
+	}
+
+}
