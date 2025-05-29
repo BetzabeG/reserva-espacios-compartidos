b71a5a06b6e030c21df6414a4d3bfc0fa02572dd
KRocioCC <krociocalderon@gmail.com>
2025-05-29 19:23:54 -0400

    feat(db): add script SQL para creación de tabla Reservador

---
+++ sistema_reservas.sql
@@ -0,0 +1,18 @@
+INSERT INTO reservador (nombre, ap_paterno, ap_materno, tipo, correo_institucional, telefono, estado_e) VALUES
+
+('Juan', 'Pérez', 'García', 'docente', 'jperez@umsa.bo', '72123456', true),
+('María', 'López', 'Rodríguez', 'estudiante', 'mlopez@umsa.bo', '73234567', true),
+('Carlos', 'González', 'Martínez', 'administrativo', 'cgonzalez@umsa.bo', '74345678', true),
+('Ana', 'Ramírez', 'Sánchez', 'docente', 'aramirez@umsa.bo', '75456789', true),
+('Luis', 'Fernández', 'Torres', 'estudiante', 'lfernandez@umsa.bo', '76567890', true),
+('Sofía', 'Morales', 'Vargas', 'administrativo', 'smorales@umsa.bo', '77678901', true),
+('Miguel', 'Díaz', 'Castillo', 'docente', 'mdiaz@umsa.bo', '78789012', true),
+('Laura', 'Silva', 'Herrera', 'estudiante', 'lsilva@umsa.bo', '79890123', true),
+('Andrés', 'Ruiz', 'Flores', 'administrativo', 'aruiz@umsa.bo', '70901234', true),
+('Paola', 'Navarro', 'Mendoza', 'docente', 'pnavarro@umsa.bo', '70123456', true),
+('Karen', 'Catari', 'Calderon', 'estudiante', 'krcatari@umsa.bo', '65135810', true);
+
+
+
+
+
